d1ee58d94d9799fc8ba67efd28a7a08b69e90a21
abid <abid.hossain@adndiginet.com>
2024-10-15 18:32:59 +0600

    tail css configuration

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,22 +1,28 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { bookListData } from "../common/api";
 
 const Home = () => {
+  const [bookListState, setBookListState] = useState([]);
   const getBookList = async () => {
     try {
       const result = await bookListData();
       console.log("5555555555555", result);
+      setBookListState(result?.results);
     } catch (error) {
       console.log(error);
     }
-    console.log('66666666666666666666666');
-    
   };
 
   useEffect(() => {
     getBookList();
   }, []);
-  return <div></div>;
+  return (
+    <div>
+      {bookListState?.map((item, index) => (
+        <li key={index}> {item?.title} </li>
+      ))}
+    </div>
+  );
 };
 
 export default Home;
